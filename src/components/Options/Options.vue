31086ef3462c441931e281219f3bdfaf15089dbe
Sil van Diepen <me@silvandiepen.com>
2020-04-27 12:14:22 +0200

    mobile fix

--- src/components/Options/Options.vue
+++ src/components/Options/Options.vue
@@ -1,6 +1,14 @@
 <template>
 	<div class="options">
-		<ul class="options__list">
+		<div
+			class="options__background"
+			@click="panelActive = !panelActive"
+			:class="{ 'options__background--active': panelActive }"
+		/>
+		<button class="options__trigger" @click="panelActive = !panelActive">
+			panel
+		</button>
+		<ul class="options__list" :class="{ 'options__list--active': panelActive }">
 			<li
 				class="options__option"
 				:class="{ 'options__option--active': list.active }"
@@ -28,7 +36,9 @@ import { CharactersType } from '@/assets/characters';
 import Vue from 'vue';
 
 export default Vue.extend({
-	name: 'HelloWorld',
+	data: () => ({
+		panelActive: false
+	}),
 	computed: {
 		charsSets(): CharactersType[] {
 			return this.$store.getters['getCharacterSets'];
@@ -44,18 +54,61 @@ export default Vue.extend({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
+@import '@/assets/scss/tools';
 .options {
 	padding: 1em;
-	background-color: rgba(80, 50, 0, 0.1);
+	width: 100%;
+	@media #{$desktop} {
+		background-color: beige();
+	}
 	li,
 	ul {
 		margin: 0;
 		list-style-type: none;
 	}
+	&__background {
+		position: fixed;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		background-color: beige(0.8);
+		opacity: 0;
+		pointer-events: none;
+		transition: opacity 0.3s;
+		@media #{$mobile} {
+			&--active {
+				opacity: 1;
+				pointer-events: all;
+			}
+		}
+	}
+	&__trigger {
+		width: 4em;
+		height: 4em;
+		border-radius: 50%;
+		position: fixed;
+		z-index: 2;
+		pointer-events: all;
+		transform: scale(0);
+		@media #{$mobile} {
+			transform: scale(1);
+		}
+	}
 	&__list {
 		padding: 1em;
 		border-radius: 1em;
 		background-color: white;
+		@media #{$mobile} {
+			max-width: 320px;
+			margin: auto;
+			box-shadow: 0 0 2em 0em beige(0.5);
+			transform: skewY(20deg) translateY(100%);
+			transition: transform 0.5s;
+		}
+		&--active {
+			transform: skewY(0deg) translateY(0%);
+		}
 	}
 	&__option {
 		border-radius: 0.5em;
@@ -66,9 +119,23 @@ export default Vue.extend({
 		& + & {
 			margin-top: 1em;
 		}
+		&::before {
+			content: '';
+			width: 1em;
+			height: 1em;
+			position: absolute;
+			left: 100%;
+			top: 50%;
+			transform: translate(-50%, -50%) rotate(45deg) scale(1);
+			border-radius: 0 0.25em 0 0;
+			background-color: blue();
+			clip-path: polygon(0 0, 100% 0, 100% 100%);
+			// transition: transform 0.3s;
+		}
 		&--active {
-			border-color: rgba(100, 200, 255, 1);
-			// color: white;
+			background-color: blue();
+			// border-color: rgba(100, 200, 255, 1);
+
 			.options__set {
 				opacity: 0.5;
 			}
@@ -95,7 +162,6 @@ export default Vue.extend({
 	}
 	&__set {
 		padding: 0;
-		max-width: 13em;
 		display: flex;
 		flex-wrap: wrap;
 		opacity: 0.25;
